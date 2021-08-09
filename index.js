bc5789dd6644f80dd865a6769ccb6364719c2516
climb719 <mary.oldweiler@gmail.com>
2021-08-09 13:37:30 -0400

    commit tree 4d934bd421814f49ee5a93919ce5f561c91df627

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => {renderBooks(json);
+  })
+  return  fetch('https://anapioficeandfire.com/api/books')
+  }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +20,29 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// function fetchBook(b) {
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => console.log(json[`${b}`]));
+// }
+
+// function fetchCharacter(c) {
+//   fetch(`https://anapioficeandfire.com/api/characters/${c}`)
+//   .then(resp => resp.json())
+//   .then(json => console.log(json));
+// }
+
+// function fetchTotalPages() {
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => {
+// const pages = []
+// for ( let i = 0; i < json.length; i++) {
+// pages.push(json[i]["numberOfPages"])
+// }
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// console.log(pages.reduce(reducer))
+// });
+// }
+
